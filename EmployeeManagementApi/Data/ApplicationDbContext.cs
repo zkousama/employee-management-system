754a967c0816dde60f088ee0e5ae77a76050e95d
ousama <ousamaistaken@gmail.com>
2024-07-13 16:04:47 +0100

    created applicationDBContext class to manage the database context

---
+++ EmployeeManagementApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagementApi.Models;
+
+namespace EmployeeManagementApi.Data {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+    public DbSet<Employee> Employees { get; set; }
+}
