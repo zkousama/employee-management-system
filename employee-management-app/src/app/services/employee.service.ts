6f5b31da9a55c94d81dce42581d351cd03902ec6
ousama <ousamaistaken@gmail.com>
2024-07-14 14:52:09 +0100

    created employeeService to handle HTTP requests

--- employee-management-app/src/app/services/employee.service.ts
+++ employee-management-app/src/app/services/employee.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private apiServerUrl = 'http://localhost:8080/api/employees';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}`);
+  }
+
+  getEmployeeById(id: string): Observable<Employee> {
+    return this.http.get<Employee>(`${this.apiServerUrl}/${id}`);
+  }
+
+  createEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}`, employee);
+  }
+
+  updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/${employee.id}`, employee);
+  }
+
+  deleteEmployee(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/${id}`);
+  }
 }
