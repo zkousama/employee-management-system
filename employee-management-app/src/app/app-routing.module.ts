f60064c827a3ab2eaf32fd71059a59cee8bed9bf
ousama <ousamaistaken@gmail.com>
2024-07-14 14:55:05 +0100

    setting up routing for components

--- employee-management-app/src/app/app-routing.module.ts
+++ employee-management-app/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'employees/new', component: EmployeeFormComponent },
+  { path: 'employees/edit/:id', component: EmployeeFormComponent },
+  { path: '', redirectTo: '/employees', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
