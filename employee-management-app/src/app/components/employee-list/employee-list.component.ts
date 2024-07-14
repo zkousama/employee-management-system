5a39e822d7803f5b674fb9eb77fa3a0045254446
ousama <ousamaistaken@gmail.com>
2024-07-14 14:53:23 +0100

    created employeeListComponent to display list of employees

--- employee-management-app/src/app/components/employee-list/employee-list.component.ts
+++ employee-management-app/src/app/components/employee-list/employee-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../models/employee.model';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
-export class EmployeeListComponent {
 
+export class EmployeeListComponent implements OnInit {
+  employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.loadEmployees();
+  }
+
+  loadEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (employees) => this.employees = employees,
+      (error) => console.error('Error loading employees:', error)
+    );
+  }
+
+  deleteEmployee(id: string): void {
+    this.employeeService.deleteEmployee(id).subscribe(
+      () => this.loadEmployees(),
+      (error) => console.error('Error deleting employee:', error)
+    );
+  }
 }
