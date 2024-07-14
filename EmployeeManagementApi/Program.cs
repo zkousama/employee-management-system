7ec4fb9223e3c5436ba77a49af0d86c536a23c9d
ousama <ousamaistaken@gmail.com>
2024-07-14 23:05:39 +0100

    refactored to include mediatR registration and handlers for employee queries & commands + added CORS policy for angular during dev

--- EmployeeManagementApi/Program.cs
+++ EmployeeManagementApi/Program.cs
@@ -1,5 +1,10 @@
-using Microsoft.EntityFrameworkCore;
+using EmployeeManagementApi.Models;
 using EmployeeManagementApi.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagementApi.Handlers;
+using EmployeeManagementApi.Queries;
+using EmployeeManagementApi.Commands;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,9 +12,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Register MediatR
+// builder.Services.AddMediatR(typeof(Program).Assembly);
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+// Register handlers
+// builder.Services.AddTransient<IRequestHandler<GetEmployeesQuery, IEnumerable<Employee>>, GetEmployeesHandler>();
+// builder.Services.AddTransient<IRequestHandler<GetEmployeeByIdQuery, Employee>, GetEmployeeByIdHandler>();
+// builder.Services.AddTransient<IRequestHandler<CreateEmployeeCommand, Employee>, CreateEmployeeHandler>();
+// builder.Services.AddTransient<IRequestHandler<UpdateEmployeeCommand, Employee>, UpdateEmployeeHandler>();
+// builder.Services.AddTransient<IRequestHandler<DeleteEmployeeCommand, bool>, DeleteEmployeeHandler>();
+// builder.Services.AddTransient<IRequestHandler<EmployeeExistsQuery, bool>, EmployeeExistsHandler>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -21,32 +48,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAngularApp"); // Add this line here
 app.UseAuthorization();
 app.MapControllers();
 
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast =  Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast")
-// .WithOpenApi();
-
 app.Run();
-
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
